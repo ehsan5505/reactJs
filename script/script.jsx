a103c4ab7c38875909b27b2df2d3f808cb8a4f89
Ehsan Rafeeque <dreamer_2142@yahoo.com>
2016-02-10 18:29:44 +0000

    add the template and view for the game:

--- script/script.jsx
+++ script/script.jsx
@@ -8,4 +8,59 @@ var Test = React.createClass({
     }
 });
 
-ReactDOM.render(<Test />,document.getElementById("container"));
+var StartFrame = React.createClass({
+    render: function(){
+        return (
+            <div id="start-frame">
+                <div className="well">
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                </div>
+            </div>
+        )
+    }
+});
+
+var ButtonFrame = React.createClass({
+    render: function(){
+        return (
+            <div id="button-frame">
+                <div className="button-frame">
+                    <button className="btn btn-primary btn-lg">=</button>
+                </div>
+            </div>
+        )
+    }
+});
+
+var AnswerFrame = React.createClass({
+    render: function(){
+        return (
+            <div id="answer-frame">
+                <div className="well">
+                    <blockquote>Sometime the act of love is hardest to commit -- Solace </blockquote>  
+                </div>
+            </div>
+        )
+    }
+});
+
+var Game = React.createClass({
+    render: function(){
+        return (
+            <div className="clearfix">
+                <h1>Nine Play</h1>
+                <hr/>
+                <StartFrame />
+                <ButtonFrame />
+                <AnswerFrame />
+            </div>
+        )
+    }
+})
+
+ReactDOM.render(<Game />,document.getElementById("container"));
