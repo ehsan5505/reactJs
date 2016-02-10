de58e6e2dc4d00d9156a13a51da112a70e84f5e8
Ehsan Rafeeque <dreamer_2142@yahoo.com>
2016-02-10 18:00:12 +0000

    add the basic template design

---
+++ script/script.jsx
@@ -0,0 +1,11 @@
+var Test = React.createClass({
+    render: function(){
+        return (
+            <div>
+                <h1>System running fine</h1>
+            </div>
+        )
+    }
+});
+
+ReactDOM.render(<Test />,document.getElementById("container"));
